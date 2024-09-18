e9d710d09caad1d86a20db4d1f6e39100f68cb39
Trey2007 <treywoods38@gmail.com>
2024-09-18 14:41:24 -0400

    Add files via upload

---
+++ P3LAB_SimmonsTreshawn.py
@@ -0,0 +1,19 @@
+# Tresahwn Simmons
+# 9/18/2024
+# P3HW2
+# Calculate coins needed for change
+
+# Floor division // Gives you back thw whole number from division
+
+
+
+
+# Modulus % Gives you back the remainder from division
+
+
+# Get amount of money
+amount = float(input("Enter the amount of money as a float: "))
+
+# Convert value to integer. Remember last two digits are still change
+int_amount = int(amount * 100
+print(int_amount)
